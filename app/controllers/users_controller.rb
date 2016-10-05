1f8363cf52c33277fcaa16172239af922442e8cb
ryosgmr <ryosgmr@tech-camp-1-023-no-MacBook-Air.local>
2016-10-06 08:50:33 +0900

    modify users/edit.html.haml

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,4 +6,14 @@ class UsersController < ApplicationController
   def edit
   end
 
+  def update
+    current_user.update(update_params)
+    redirect_to root_path
+  end
+
+  private
+  def update_params
+    params.require(:user).permit(:name, :email, :member_of, :profile, :works, :password)
+  end
+
 end
