4da6365537a6f3f77cdeaa9ee2083e8e824fc664
ryosgmr <ryosgmr@tech-camp-1-023-no-MacBook-Air.local>
2016-10-06 21:53:52 +0900

    modify devise's config for validate password length

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,7 +8,7 @@ class UsersController < ApplicationController
 
   def update
     current_user.update(update_params)
-    sign_in(current_user, bypass: true)
+    bypass_sign_in(current_user)
     redirect_to root_url
   end
 
