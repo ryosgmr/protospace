b0cce1268c38070c51489cace36b3d92ab861a97
ryosgmr <ryosgmr@tech-camp-1-023-no-MacBook-Air.local>
2016-10-15 18:32:56 +0900

    add reject_if for images in prototype model and use user association

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,6 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
-    @prototypes = Prototype.where(user_id: @user)
   end
 
   def edit
--- app/models/prototype.rb
+++ app/models/prototype.rb
@@ -1,6 +1,6 @@
 class Prototype < ActiveRecord::Base
   has_many :captured_images
   belongs_to :user
-  accepts_nested_attributes_for :captured_images
+  accepts_nested_attributes_for :captured_images, reject_if: proc { |attributes| attributes['image'].blank? }
   validates :title, :catch_copy, :concept, presence: true
 end
